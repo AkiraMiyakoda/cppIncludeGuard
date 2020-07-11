d4ce21c146b43ef529d8ee9aa66844ec4f3f47bd
Akira Miyakoda <akira.miyakoda@gmail.com>
2020-07-11 20:04:08 +0900

    Apply Prittier to TypeScript files.

--- src/extension.ts
+++ src/extension.ts
@@ -5,20 +5,25 @@
  * https://opensource.org/licenses/MIT
  */
 
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('Congratulations, your extension "cppIncludeGuard" is now active!');
+  console.log(
+    'Congratulations, your extension "cppIncludeGuard" is now active!'
+  );
 
-    const commands = require('./commands');
-    context.subscriptions.push(
-        vscode.commands.registerCommand(
-            'extension.insertIncludeGuard', () => commands.insertIncludeGuard()),
-        vscode.commands.registerCommand(
-            'extension.removeIncludeGuard', () => commands.removeIncludeGuard()),
-        vscode.commands.registerCommand(
-            'extension.updateIncludeGuard', () => commands.updateIncludeGuard())
-    );
+  const commands = require("./commands");
+  context.subscriptions.push(
+    vscode.commands.registerCommand("extension.insertIncludeGuard", () =>
+      commands.insertIncludeGuard()
+    ),
+    vscode.commands.registerCommand("extension.removeIncludeGuard", () =>
+      commands.removeIncludeGuard()
+    ),
+    vscode.commands.registerCommand("extension.updateIncludeGuard", () =>
+      commands.updateIncludeGuard()
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
